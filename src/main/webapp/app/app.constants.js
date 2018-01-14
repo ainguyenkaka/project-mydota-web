ee20aa03bbae5d50cf614ccd4ec86ca384f12ad4
Ai Nguyen <ainguyenkaka@gmail.com>
2018-01-14 11:29:07 +0800

    add tag manager

--- src/main/webapp/app/app.constants.js
+++ src/main/webapp/app/app.constants.js
@@ -5,6 +5,6 @@
         .module('webApp')
         .constant('VERSION', "0.0.1-SNAPSHOT")
         .constant('DEBUG_INFO_ENABLED', false)
-        .constant('BUILD_TIMESTAMP', 1515897870902)
+        .constant('BUILD_TIMESTAMP', 1515900508920)
 ;
 })();
